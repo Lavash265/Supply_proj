c72ce7184cc6f288cd2ba406749be1f0a15ee051
Lavash265 <120392037+Lavash265@users.noreply.github.com>
2024-06-06 13:35:23 +0300

    Add files via upload

---
+++ supplyes_project/src/main/java/org/example/supplyes_project/interfaces/DeliveryService.java
@@ -0,0 +1,13 @@
+package org.example.supplyes_project.interfaces;
+
+import org.example.supplyes_project.models.Delivery;
+
+import java.util.List;
+
+public interface DeliveryService {
+    void addDelivery(Delivery delivery);
+    List<Delivery> getAllDeliveries();
+    Delivery getDeliveryById(int id);
+    void updateDelivery(Delivery delivery);
+    void deleteDelivery(int id);
+}
