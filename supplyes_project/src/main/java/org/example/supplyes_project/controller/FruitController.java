9a6588d917c3fc0eaf27e1badfe57cb348be24e4
Lavash265 <120392037+Lavash265@users.noreply.github.com>
2024-06-06 11:40:30 +0300

    Add files via upload

---
+++ supplyes_project/src/main/java/org/example/supplyes_project/controller/FruitController.java
@@ -0,0 +1,42 @@
+package org.example.supplyes_project.controller;
+
+
+import org.example.supplyes_project.interfaces.FruitService;
+import org.example.supplyes_project.models.Fruit;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/fruits")
+public class FruitController {
+
+    @Autowired
+    private FruitService fruitService;
+
+    @PostMapping("/")
+    public void addFruit(@RequestBody Fruit fruit) {
+        fruitService.addFruit(fruit);
+    }
+
+    @GetMapping("/getAllFruits")
+    public List<Fruit> getAllFruits() {
+        return fruitService.getAllFruits();
+    }
+
+    @GetMapping("/{id}")
+    public Fruit getFruitById(@PathVariable int id) {
+        return fruitService.getFruitById(id);
+    }
+
+    @PutMapping("/")
+    public void updateFruit(@RequestBody Fruit fruit) {
+        fruitService.updateFruit(fruit);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteFruit(@PathVariable int id) {
+        fruitService.deleteFruit(id);
+    }
+}
---
+++ supplyes_project/src/main/java/org/example/supplyes_project/models/Fruit.java
@@ -0,0 +1,52 @@
+package org.example.supplyes_project.models;
+
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Fruit")
+public class Fruit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "fruit_id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price_per_kg")
+    private double pricePerKg;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPricePerKg() {
+        return pricePerKg;
+    }
+
+    public void setPricePerKg(double pricePerKg) {
+        this.pricePerKg = pricePerKg;
+    }
+
+
+}
