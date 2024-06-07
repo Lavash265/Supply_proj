bf09ce530afc146644aa8acecab0a2a2fc8a6b13
Lavash265 <120392037+Lavash265@users.noreply.github.com>
2024-06-07 20:11:10 +0300

    Add files via upload

---
+++ supplyes_project/src/main/java/org/example/supplyes_project/models/Delivery.java
@@ -0,0 +1,74 @@
+package org.example.supplyes_project.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "delivery")
+public class Delivery {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "supplier_id")
+    private Supplier supplier;
+
+    @ManyToOne
+    @JoinColumn(name = "fruit_id")
+    private Fruit fruit;
+
+    @Column(name = "delivery_date")
+    private LocalDateTime deliveryDate;
+
+    @Column(name = "weight")
+    private double weight;
+
+    public Supplier getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(Supplier supplier) {
+        this.supplier = supplier;
+    }
+
+    public Fruit getFruit() {
+        return fruit;
+    }
+
+    public void setFruit(Fruit fruit) {
+        this.fruit = fruit;
+    }
+
+    public LocalDateTime getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public void setDeliveryDate(LocalDateTime deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
+
+
+
---
+++ supplyes_project/src/main/java/org/example/supplyes_project/repository/ReportRepository.java
@@ -0,0 +1,12 @@
+package org.example.supplyes_project.repository;
+
+import org.example.supplyes_project.models.ReportEntry;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+
+public interface ReportRepository {
+    List<ReportEntry> generateReportByDateRange(LocalDateTime startDate, LocalDateTime endDate);
+
+}
